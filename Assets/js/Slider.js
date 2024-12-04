241e8a2da2a50a3e0ef13ca620f4ed27c56ba96d
Hafilue c <hafiluecmuble@gmail.com>
2024-12-04 16:30:29 +0530

    stack card on scroll in sustainable section

--- Assets/js/Slider.js
+++ Assets/js/Slider.js
@@ -68,42 +68,7 @@ $(".int-sliders").slick({
   ],
 });
 
-$(".effort-slider").slick({
-  vertical: true,
-  verticalSwiping: true,
-  infinite: true,
-  slidesToShow: 1.03,
-  slidesToScroll: 1,
-  autoplay: true,
-  dots: false,
-  draggable: false,
-  pauseOnHover: false,
-  pauseOnFocus: false,
-  cssEase: "ease-in-out",
-  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
-  responsive: [
-    {
-      breakpoint: 1300,
-      settings: { slidesToShow: 1.01, slidesToScroll: 1 },
-    },
-    {
-      breakpoint: 992,
-      settings: { slidesToShow: 1.01, slidesToScroll: 1 },
-    },
-    {
-      breakpoint: 960,
-      settings: { slidesToShow: 1.01, slidesToScroll: 1 },
-    },
-    {
-      breakpoint: 765,
-      settings: { slidesToShow: 1.01, slidesToScroll: 1 },
-    },
-    {
-      breakpoint: 540,
-      settings: { slidesToShow: 1.01, slidesToScroll: 1 },
-    },
-  ],
-});
+ 
 
 $(".blog-Sliders").slick({
   infinite: true,
