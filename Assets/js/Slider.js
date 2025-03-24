11cd60e697c4e38a78d602af6ccbe379910ef4e9
Hafilue c <hafiluecmuble@gmail.com>
2025-03-24 18:21:36 +0530

    about page added

--- Assets/js/Slider.js
+++ Assets/js/Slider.js
@@ -104,3 +104,82 @@ $(".blog-Sliders").slick({
     },
   ],
 });
+
+
+
+// about sliders
+
+
+$(".photos-Sliders").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  dots: false,
+  draggable: false,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  cssEase: "ease-in-out",
+  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+  responsive: [
+    {
+      breakpoint: 1030,
+      settings: { slidesToShow: 3, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 992,
+      settings: { slidesToShow: 2, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 960,
+      settings: { slidesToShow: 2, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 765,
+      settings: { slidesToShow: 2, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 540,
+      settings: { slidesToShow: 1, slidesToScroll: 1 },
+    },
+  ],
+});
+
+
+// mentor sliders
+
+
+$(".mentor-Sliders").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  dots: false,
+  draggable: false,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+  cssEase: "ease-in-out",
+  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+  responsive: [
+    {
+      breakpoint: 1300,
+      settings: { slidesToShow: 3, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 992,
+      settings: { slidesToShow: 2, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 960,
+      settings: { slidesToShow: 2, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 765,
+      settings: { slidesToShow: 2, slidesToScroll: 1 },
+    },
+    {
+      breakpoint: 540,
+      settings: { slidesToShow: 1, slidesToScroll: 1 },
+    },
+  ],
+});
